d66f419555490bce533aa594050f697ecc58d417
Shane Kennedy <kennedy.shane@att.net>
2022-06-10 13:40:27 -0500

    General updates & finished RNG, started calculator

--- assets/js/basics.js
+++ assets/js/basics.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
-    
+    $("#calculateRNG").click(function(e) {
+        const max = parseInt($("#maxInput")[0].value);
+        const min = parseInt($("#minInput")[0].value);
+        let res = Math.floor(Math.random()*max);
+        if(min < max && $("#maxInput")[0].value.length <= 6 && $("#minInput")[0].value <= 6) {
+            while(res < min) {
+                res = Math.floor(Math.random()*max)+1
+            }
+            $("#result")[0].innerHTML = res;
+        } else {
+            $("#result")[0].innerHTML = "N/A";
+        }
+    })
 })
